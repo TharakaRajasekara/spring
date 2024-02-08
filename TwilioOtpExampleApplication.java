f5953a5a325d00e45a071980aa10989779eb4810
Tharaka Rajasekara <124290593+TharakaRajasekara@users.noreply.github.com>
2024-02-08 19:44:55 +0530

    Add files via upload

---
+++ TwilioOtpExampleApplication.java
@@ -0,0 +1,28 @@
+package com.javatechie.twilio;
+
+import com.javatechie.twilio.config.TwilioConfig;
+import com.twilio.Twilio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+@EnableSwagger2
+public class TwilioOtpExampleApplication {
+
+	@Autowired
+	private TwilioConfig twilioConfig;
+
+	@PostConstruct
+	public void initTwilio(){
+		Twilio.init(twilioConfig.getAccountSid(),twilioConfig.getAuthToken());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilioOtpExampleApplication.class, args);
+	}
+
+}
